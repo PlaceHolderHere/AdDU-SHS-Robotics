1cb27b1d9a26f7428824323fbfe4587dff133cb8
PlaceHolderHere <holderhereinplace@gmail.com>
2026-06-17 16:43:21 +0800

    Added Home Section and comwiz-logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
+import { Button } from "./Components/Button"
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="app">
+        <section id="home" className="home">
+            <img className="home-logo" src="/comwiz-svg.svg" alt="Computer Wizards Logo"/>
+            <h1 className="home-title">Computer Wizards</h1>
+            <Button text="Learn more->"></Button>
+        </section>
+
+        <section id="objectives" className="objectives">
+            <h2>Hello</h2>
+        </section>
+    </div>
   )
 }
 
